4990d3d4ad1b973f882e007f231d0c4f57b1db2e
scar17off <scar17off@gmail.com>
2025-02-20 08:23:08 +0200

    Add Forsaken game script with ESP functionality

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ A Roblox script hub with support for multiple games.
 
 - [Arsenal](places/286090429.lua) - `286090429`
 - [AniPhobia](places/6788434697.lua) - `6788434697`
-
+- [Forsaken](places/18687417158.lua) - `18687417158`
 ## Usage
 
 ```lua
---
+++ places/18687417158.lua
@@ -0,0 +1,47 @@
+local UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/scar17off/scarhack/refs/heads/main/libraries/ui-library.lua"))()
+local ESPLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/scar17off/scarhack/refs/heads/main/libraries/esp.lua"))()
+
+local window = UI.CreateWindow()
+
+-- Categories
+local ESP = window:CreateCategory("ESP")
+
+ESP:AddObjectListener(workspace.Map.Ingame.Map, {
+    Name = "Generator",
+    CustomName = "Generators",
+    Color = Color3.fromRGB(255, 255, 0),
+    IsEnabled = "Generator"
+})
+
+local espToggle = ESP:CreateToggle({
+    text = "ESP",
+    default = false,
+    callback = function(value)
+        ESPLibrary.Players = value
+        ESPLibrary:Toggle(value)
+    end
+})
+
+espToggle:AddToggle({
+    text = "Boxes",
+    default = false,
+    callback = function(value)
+        ESPLibrary.Boxes = value
+    end
+})
+
+espToggle:AddToggle({
+    text = "Generators",
+    default = false,
+    callback = function(value)
+        ESPLibrary.Generators = value
+    end
+})
+
+espToggle:AddToggle({
+    text = "Killers",
+    default = false,
+    callback = function(value)
+        -- ESPLibrary.Killers = value
+    end
+})
